3ebf96a49b5967e5499473d0d86b42e9902694af
PicotPernel <lolpernelestla@gmail.com>
2025-01-13 09:07:08 +0100

    Ajout des formulaires

--- ajouterlivre.php
+++ ajouterlivre.php
@@ -1,4 +1,23 @@
 <?php
-include 'entete.php';
+include 'enteteadmin.php';
 include 'authentification.php';
 ?>
+<div class="row">
+    <div class=col-1>
+    </div>
+    <div class=col-8>
+        <form>
+            <h2>Titre</h2>    
+            <input type="text" name="titre">
+            <br>
+            <h2>Isbn13</h2>   
+            <input type="text" name="isbn13">
+            <br>
+            <h2>Année de parution</h2> 
+            <input type="text" name="anneparution">
+            <br>
+            <h2>Résumé</h2>
+            <textarea class="form-control" rows="5" id="comment" name="resume"></textarea>
+        </form>
+    </div>
+</div>
--- ajouterutilisateur.php
+++ ajouterutilisateur.php
@@ -1,4 +1,34 @@
 <?php
 include 'enteteadmin.php';
-
+include 'authentification.php';
 ?>
+<div class="row">
+    <div class=col-1>
+    </div>
+    <div class=col-8>
+        <form>
+            <h2>Email</h2>    
+            <input type="text" name="mél">
+            <br>
+            <h2>Mot de passe</h2>   
+            <input type="text" name="motdepasse">
+            <br>
+            <h2>Nom</h2> 
+            <input type="text" name="nom">
+            <br>
+            <h2>Prénom</h2>    
+            <input type="text" name="prénom">
+            <br>
+            <h2>Adresse</h2>   
+            <input type="text" name="adresse">
+            <br>
+            <h2>Ville</h2> 
+            <input type="text" name="ville">
+            <br>
+            <h2>Code postal</h2>    
+            <input type="text" name="codepostal">
+            <br>
+            <input type="submit" value="Ajouter" name="AjoutUtilisateur">
+        </form>
+    </div>
+</div>
